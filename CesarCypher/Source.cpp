0e52f3205890edb9a73ade190d74559fa927cfb0
root <root@WALL-E>
2017-05-29 20:03:32 -0400

    Refactor the code to use the ascii value of characters.

--- CesarCypher/Source.cpp
+++ CesarCypher/Source.cpp
@@ -1,57 +1,39 @@
 /*
-Title: C++Cipher.
+Title: C++ CesarCipher.
 Author: Sorin Diaconescu
-Description:
-	This is a representation in C++ of the Cesar Cipher.
 */
+
 #include <iostream>
-#include <string.h>
-#include <vector>
+#include <sstream>
 
 
-int main(int argc, char ** argv)
+
+int main()
 {
-	int key;
-	char message[256];
-	
-	std::cout<< "Enter a key to encrypt: ";
-	std::cin>> key;
-		
-	std::cout<< "Enter your message: ";
-	std::cin>> message;
-	
-	std::string alphaSpace = " abcdefghijklmnopqrstuvwxyz";          // Store the characters used in a char array.
-	int alphaSpaceLenght = std::size(alphaSpace);                    // Store the lenght of the character array.
+	int encryptionKey;													// Store the encription key
+	std::cout << "Enter a key to encrypt:" << std::endl;
+	std::cin >> encryptionKey;
 
-	std::vector<int> list;                                           // Define an empty vector which will later contain a list of leter indexes used to encrypt the message.
-	std::vector<char> encMSG;                                        // Define an empty vector which will later contain the encripted message.
+	std::string rawMessage;												// Store the unencrypted message.
+	std::cout << "Enter your message: " << std::endl;
+	std::getline(std::cin >> std::ws, rawMessage);
 
-	for (int l = 0; message[l]; ++l) {                               // Iterate through the characters of the message.
-		char letter = message[l];                                    // Store each character in a variable "letter" for later comparison.
-		for (int c = 0; alphaSpace[c]; ++c) {                        // Iterate through the caracters of the alphaSpace.
-			if (letter == alphaSpace[c]) {                           // Test if the letter matches a character in alphaSpace.
-				int shiftedIndexes = c + key;                        // Store the intex of the matching letter and increment it by the shiftting value.
-				if (shiftedIndexes > alphaSpaceLenght) {
-					shiftedIndexes = (shiftedIndexes - alphaSpaceLenght) - 1 ;
-				}
-				list.insert(list.end(), shiftedIndexes);
-				break;                                               // Break out of the loop.
-			}
-		}
+	std::string encriptedMessage;										// Define an empty vector which will later contain the encripted message.
+
+	for (int i = 0; rawMessage[i]; ++i)									// Iterate through the characters of the message.
+	{
+		//char asciiLetter = rawMessage[i];														    // Store each character in a variable "asciiLetter" for conversion.
+		//int asciiLetterIndex = static_cast<int>(asciiLetter);										// Convert the character into ascii value.
+		//int newAsciiLetterIndex = static_cast<int>(asciiLetter) + encryptionKey;					// Apply the encryptionKey the ascii value of the character.
+		//char newAsciiLetter = static_cast<char>(static_cast<int>(asciiLetter) + encryptionKey);	// Convert the ascii value into the new character.
+
+		encriptedMessage.push_back(static_cast<char>(static_cast<int>(rawMessage[i]) + encryptionKey));
+		/* Calculate the new ascii value by applying the encryptionKey to the caracter's ascii original value 
+		and then append the new character to the encriptedMessage string.
+		*/
 	}
 
-	std::vector<int>::iterator listBegin = list.begin();
-	std::vector<int>::iterator listEnd = list.end();
-	for (auto listb = listBegin; listb < listEnd; ++listb) {
-		for (int i = 0; alphaSpace[i]; ++i) {
-			if (i == *listb) {
-				encMSG.insert(encMSG.end(), alphaSpace[i]);
-				break;
-			}
-		}
-	}
-
-	std::string encriptedMessage(encMSG.begin(), encMSG.end());
-	printf("Your Encripted message is: %s", encriptedMessage.c_str());
-
+	std::cout << encriptedMessage << std::endl;;
+	system("pause");
+	return 0;
 }
