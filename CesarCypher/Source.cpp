2d0279c547822e37849b6c2df9418a7dc34236bb
root <root@WALL-E>
2017-05-30 21:41:03 -0400

    arguments key and help now working.

--- CesarCypher/Source.cpp
+++ CesarCypher/Source.cpp
@@ -7,66 +7,82 @@ Author: Sorin Diaconescu
 #include <sstream>
 #include <string>
 #include <vector>
+#include <typeinfo>
 
-static void show_usage( std::string name )
+using namespace std;
+
+static void show_usage( string name )
 {
-	std::cerr << "Usage: " << name << " Options:\n"
+	cerr << "Usage: " << name << " Options:\n"
 		<< "\t-h, --help\tShow this help message.\n"
 		<< "\t-e, --encrypt \tUse this option to encrypt a message.\n" 
 		<< "\t-d, --decrypt \tUse this option to decrypt message.\n" 
 		<< "\t-k, --key \tSpecify a key form 1 to 100 to encrypt your message.\n";
 }
 
+string getMessage() {
+	string msg;
+	cout << "Enter your message: \n";
+	getline(cin >> ws, msg);
+	return msg;
+}
+
+int getKey() {
+	int k;
+	cout << "Enter a key to encrypt:\n";
+	cin >> k;
+	return k;
+}
+
+string encrypt( int k, string m ) {
+	string msg;															// Define an empty string which will later contain the encripted message.
+	for (int i = 0; m[i]; ++i)											// Iterate through the characters of the message.
+	{
+		/* Calculate the new ascii value by applying the encryptionKey to the caracter's ascii original value
+		and then append the new character to the encriptedMessage string.
+		*/
+		msg.push_back(static_cast<char>(static_cast<int>(m[i]) + k));
+	}
+	return msg;
+}
 
 int main( int argc, char *argv[] ) {
-	if ( argc < 2 ) {
-		show_usage( argv[0] );
-		return 1;
+
+	int encryptionKey;
+	string rawMessage;
+	string encryptedMsg;
+
+	if (argc == 1) {
+		encryptionKey = getKey();
+		rawMessage = getMessage();
+		encryptedMsg = encrypt(encryptionKey, rawMessage);
+		cout << encryptedMsg << endl;
+		return 0;
 	}
 
-
-
 	for ( int i = 1; i < argc; ++i ) {
-		std::string arg = argv[i];
+		string arg = argv[i];
 		if ( ( arg == "-h" ) || ( arg == "--help" ) ) {
 			show_usage(argv[0]);
 			return 0;
 		}
 		else if ( ( arg == "-k" ) || ( arg == "--key" )) {
 			if  ( i + 1 < argc ) {											// Make sure we aren't at the end of argv!
-				std::string key = argv[i + 1];								// Increment 'i' so we don't get the argument as the next argv[i].
-				std::cout << key << std::endl;
-				int encryptionKey = static_cast<int>(key);
-
+				string k = argv[i + 1];										// Increment 'i' to get the next argument as the value for option -k / --key.
+				encryptionKey = atoi(k.c_str());							// I have no clue what atoi nor c_str() do. My assumptio is that it converts stuf to other stuff ...DOOHH!!!
+				if ( encryptionKey < 1 ) {
+					cout << "option --key requires a integer value.\n";
+					return 0;
+				}
+				rawMessage  = getMessage();
+				encryptedMsg = encrypt(encryptionKey, rawMessage);
+				cout << encryptedMsg << endl;
 			}
-			else {															// There was no argument to the key option.
-				std::cerr << "--key option requires one argument.\n";
-				return 1;
+			else {
+				cout << "option --key requires a integer value.\n";
+				return 0;
 			}
 		}
-		else {
-			std::cout << "Some code here.\n";
-		}
 	}
-
-	std::cout << "Enter a key to encrypt:\n";
-	std::cin >> encryptionKey;
-
-	std::string rawMessage;													// Store the unencrypted message.
-	std::cout << "Enter your message: \n";
-	std::getline( std::cin >> std::ws, rawMessage );
-
-	std::string encriptedMessage;										 	// Define an empty string which will later contain the encripted message.
-
-	for ( int i = 0; rawMessage[i]; ++i )									// Iterate through the characters of the message.
-	{
-		/* Calculate the new ascii value by applying the encryptionKey to the caracter's ascii original value
-		and then append the new character to the encriptedMessage string.
-		*/
-		encriptedMessage.push_back( static_cast<char>( static_cast<int>( rawMessage[i]) + encryptionKey ) );
-	}
-
-	std::cout << encriptedMessage << std::endl;;
-	//system("pause");
 	return 0;
 }
